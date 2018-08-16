b322104d5ff15e171aaf380c5a2c329511bafbec
dingzu <349923359@qq.com>
2018-08-16 09:09:53 +0800

    修改首页

--- docs/.vuepress/theme/v2/Home.vue
+++ docs/.vuepress/theme/v2/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>百度云设计规范网站 V3</h1>
+    <p>页面从代码仓库自动构建</p>
+    <a target="blank"
+    style="font-size:24px;"
+    href="https://github.com/dingzu/acuStd3/">
+    打开代码仓库</a>
+    <hr style="margin:40px 0 40px;">
     <router-link :to="'/portal/'" class="home-link">
     portal 门户规范
     </router-link>
@@ -30,6 +36,9 @@ export default {
     color #000
     margin-top 40px
     display block
+  p
+    text-align center
+    color red
 </style>
 
 
